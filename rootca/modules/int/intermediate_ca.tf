f5bf32ecc0833a9e0ed024901fe45aed1330bec3
nolon <nolon@gmx.net>
2020-06-22 13:47:00 +0200

    Add K8s auth roles

--- rootca/modules/int/intermediate_ca.tf
+++ rootca/modules/int/intermediate_ca.tf
@@ -16,7 +16,7 @@ resource "vault_pki_secret_backend_intermediate_cert_request" "intermediate" {
   backend = vault_mount.pki_int.path
   #backend = var.vault_mount_root
   type = "exported"
-  # This appears to be overwritten when the CA signs this cert, I'm not sure 
+  # This appears to be overwritten when the CA signs this cert, I'm not sure
   # the importance of common_name here.
   common_name = "Intermediate Certificate"
   #common_name = "${var.server_cert_domain} Intermediate Certificate"
@@ -34,17 +34,16 @@ resource "vault_pki_secret_backend_root_sign_intermediate" "intermediate" {
   backend = var.vault_mount_root
 
   csr = vault_pki_secret_backend_intermediate_cert_request.intermediate.csr
-  common_name = "var.server_cert_domain Intermediate Certificate"
+  common_name = var.server_cert_domain
   exclude_cn_from_sans = true
   ou                   = var.ou
   organization         = var.organization
   # Note that I am asking for 8 years here, since the vault_mount.root has a max_lease_ttl of 5 years
   # this 8 year request is shortened to 5.
   ttl = 252288000 #8 years
- 
 }
 
-resource "vault_pki_secret_backend_role" "k8s_master_role" {
+resource "vault_pki_secret_backend_role" "master" {
   backend  = vault_mount.pki_int.path
 
   name           = "master"
@@ -55,57 +54,62 @@ resource "vault_pki_secret_backend_role" "k8s_master_role" {
   ttl     = var.cert_ttl
 
   allowed_domains = [
-  | "kube-apiserver",
-  | "kube-apiserver-kubelet-client"
+   "kube-apiserver",
+   "kube-apiserver-kubelet-client"
   ]
 
-  allowed_uri_sans = concat(each.value.apiserver_hostnames, [
-  | "kubernetes.default.svc.cluster.local",
-  | "kubernetes.default.svc.cluster",
-  | "kubernetes.default.svc",
-  | "kubernetes.default",
-  | "kubernetes",
-  ])
+  #allowed_uri_sans = concat(apiserver_hostnames, [
+  allowed_uri_sans = [
+   "kubernetes.default.svc.cluster.local",
+   "kubernetes.default.svc.cluster",
+   "kubernetes.default.svc",
+   "kubernetes.default",
+   "kubernetes",
+  ]
 
   key_usage = ["DigitalSignature", "KeyEncipherment"]
   # ServerAuth is required for kube-apiserver cert, ClientAuth for kube-apiserver-kubelet-client
   ext_key_usage = ["ServerAuth", "ClientAuth"]
 }
 
-resource "vault_policy" "pki_role" {
-
-  name = "pki_sign"
+resource "vault_policy" "issue_cert" {
+  name = "issue_cert"
 
   policy = <<EOT
-path "${vault_mount.pki_int.path}/issue/${vault_pki_secret_backend_role.etcd_master_role[each.key].name}"
+path "${vault_mount.pki_int.path}/issue/${vault_pki_secret_backend_role.master.name}"
  {
   capabilities = ["create", "update"]
 }
-}
 EOT
-
-resource "vault_approle_auth_backend_role" "master_authrole" {
-  for_each = var.clusters
-  backend  = var.approle_path
-
-  role_name = "${each.key}_master"
-  # we do not use a secret id and instead bind localhost as cidr for demo purposes
-  bind_secret_id    = false
-  token_bound_cidrs = ["127.0.0.1/32"]
-  token_policies    = [vault_policy.pki_role_access.name]
 }
 
+resource "vault_token_auth_backend_role" "issue-cert" {
+  role_name = "issue-cert"
+  allowed_policies    = [vault_policy.issue_cert.name]
+  token_policies    = [vault_policy.issue_cert.name]
+  #token_policies    = [vault_policy.issue_certpki_role.name]
+}
 
-resource "vault_pki_secret_backend_intermediate_set_signed" "intermediate" { 
+resource "vault_kubernetes_auth_backend_role" "issue-cert" {
+  backend                          = var.kubernetes_auth_path
+  role_name                        = "issue-cert"
+  bound_service_account_names      = ["${var.kubernetes_sa}"]
+  bound_service_account_namespaces = ["${var.kubernetes_namespace}"]
+  token_ttl                        = 3600
+  token_policies                   = [vault_policy.issue_cert.name]
+  #token_policies                   = ["default", "issue-cert"]
+}
+
+resource "vault_pki_secret_backend_intermediate_set_signed" "intermediate" {
  backend = vault_mount.pki_int.path
 
  certificate = vault_pki_secret_backend_root_sign_intermediate.intermediate.certificate
 }
 
 output "intermediate_ca" {
-    value = vault_pki_secret_backend_root_sign_intermediate.intermediate.certificate
+  value = vault_pki_secret_backend_root_sign_intermediate.intermediate.certificate
 }
 
 output "intermediate_key"  {
-    value = "${vault_pki_secret_backend_intermediate_cert_request.intermediate.private_key}"
+  value = "${vault_pki_secret_backend_intermediate_cert_request.intermediate.private_key}"
 }
--- rootca/modules/int/variables.tf
+++ rootca/modules/int/variables.tf
@@ -8,11 +8,15 @@ variable "server_cert_domain" {
   description = "Cert domain"
 }
 
-variable "pki_path" {
+variable "kubernetes_namespace" {
   type        = string
   description = "PKI path"
 }
 
+variable "kubernetes_sa" {
+  type        = string
+  description = "PKI path"
+}
 
 variable "organization" {
   type        = string
@@ -22,4 +26,9 @@ variable "organization" {
 variable "ou" {
   type        = string
   description = "ou for generated CAs"
+}
+
+variable "cert_ttl" {
+  type        = string
+  description = "INT Cert TTL"
 }
